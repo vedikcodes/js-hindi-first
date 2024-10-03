7f7a7b9643ae63dd19502f891234fa858b53f792
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-10-03 04:57:15 +0000

    learn variables in js

---
+++ basics/01_variable.js
@@ -0,0 +1,11 @@
+const password = 1435
+let emailId = "vediktiwari23@gmail.com"
+var accountId = "12222"
+accountCity = "jabalpur"
+let accountState ;
+
+/*prefer not to use var
+because of isssue in functional scope and block scope
+*/
+
+console.table ([password,emailId , accountId, accountCity , accountState])
