2713152d69e26e097763ac146a74ced365c371fa
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-10-03 13:18:08 +0000

    datatype

---
+++ basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs not browser
+
+console.log( 3 +
+    3
+)  //code readability should be high
+
+let name = "vedik" //string datatype
+let age = 22       //int datatype
+let isLoggedIn = true //boolean datatype
+
+console.log (isLoggedIn)
+console.table([name , age , isLoggedIn]) 
+
+//number 2 to the power 53
+//bigint
+//string ==> ""
+//boolean ==> true or false
+//null is a standalone value
+//undefined
+//symbol ==> unique
+
+
+
+//objects
+
+console.log( typeof "vedik")
+console.log( typeof null)
+console.log( typeof undefined)
