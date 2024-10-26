8dd45c37a248ae5e49fc953d8ebceef1b1b6bb63
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-10-26 04:45:26 +0000

    date n Time in js

---
+++ basics/07_date.js
@@ -0,0 +1,23 @@
+// DATES
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString())
+
+console.log( typeof myDate) //object
+
+let myCreateDate = new Date(2024 , 09,27)
+console.log(myCreateDate.toDateString()) // Mon Jan 23 2023
+
+let myTimeStamp =Date.now()
+//console.log(myTimeStamp) //millisecond value
+//console.log(myCreateDate.getTime())
+ //console.log(Math.floor(Date.now()/1000))
+
+ 
+
+
+
