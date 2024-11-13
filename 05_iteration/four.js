efbb90abe736e9e1526cf43f59760c2975f95026
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-11-13 11:15:59 +0000

    for in loops

---
+++ 05_iteration/four.js
@@ -0,0 +1,15 @@
+// const myObject = {
+//     js :'javascript',
+//     cpp :'c++',
+//     rb : 'ruby',
+//     swift : "swipt by apple"
+// }
+
+
+// for in loop
+
+
+const programming = ["js" , "cpp" ,"ruby"]
+for(const key in programming){
+    console.log(key + ": " +programming[key]);
+}
