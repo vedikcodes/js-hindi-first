133fae453c4ce2c83eecfe2dcbcc345a0cf0789e
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-11-13 17:32:29 +0000

    more in js

---
+++ 05_iteration/six.js
@@ -0,0 +1,28 @@
+// const coding = ["js" , "ruby" , "python" ,"java", "c++"]
+
+// const values = coding.forEach( (item) => {
+
+//     console.log(item)
+    
+// })
+// console.log(values);
+
+
+const myNums = [1 , 2, 3, 4, 5, 6, 7, 8, 9]
+//const newNums =myNums.filter( (num) => num >4 )
+//console.log(newNums);
+
+// const newNums =myNums.filter( (num) =>{
+//      return num >4 
+// }  )
+
+const newNums = []
+myNums.forEach( (num) => {
+    if(num>4) {
+        newNums.push(num)
+    }
+})
+console.log(newNums);
+
+
+
