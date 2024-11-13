0df2ae5fe2c0a693faf736154dbc9ab3c990d093
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-11-13 18:15:20 +0000

    more on js

---
+++ 05_iteration/eight.js
@@ -0,0 +1,32 @@
+const myNums =[1,2,3,4]
+
+// const myTotal =myNums.reduce(function(acc , currval) {
+//     console.log(`acc:${acc} and currval: ${currval}`);
+    
+//     return acc + currval
+// } , 0)
+
+const myTotal = myNums.reduce((acc , curr) => acc+curr , 0)
+console.log(myTotal);
+
+const shoppingCart = [ {
+    itemName : "js course"
+    price : 2999 
+},
+
+    {
+        itemName : "java course"
+        price : 9999
+     },
+     {
+        itemName : "py course"
+        price : 999
+     },
+    
+
+
+]
+
+
+const priceToPay =shoppingCart.reduce((acc ,item) => acc+ item.price , 0)
+console.log(priceToPay)
