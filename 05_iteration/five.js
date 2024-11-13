e06db9d85d2714c8e5148d2de09cf356ef222087
Vedik Tiwari <165828663+vedikcodes@users.noreply.github.com>
2024-11-13 11:34:45 +0000

    forEach in js

---
+++ 05_iteration/five.js
@@ -0,0 +1,46 @@
+const coding = ["js" , "ruby" , "python" ,"java", "c++"]
+// coding.forEach( function (val){
+//     console.log(val);
+    
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item , index , arr);
+    
+// })
+
+const myCoding = [
+    {
+    languageName :"javascript",
+    languageFileName : "js"
+},
+
+{
+    languageName :"java",
+    languageFileName : "java"
+},
+{
+    languageName :"c++",
+    languageFileName :"c"
+},
+
+
+]
+
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
